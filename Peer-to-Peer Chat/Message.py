17e9b0d5fbd5ab733e33d3e64c66c42240f9b083
Christopher Whitney <chrisswhitneyy@gmail.com>
2017-02-09 09:36:40 -0700

    skeleton of classes

--- Peer-to-Peer Chat/Message.py
+++ Peer-to-Peer Chat/Message.py
@@ -3,6 +3,36 @@
 
 class Message(object):
     """docstring for Message."""
+
+    _type_id = ''
+    _from_id = ''
+    _from_user_id = ''
+    _data = ''
+
+    def get_type_id ():
+        return self._type_id
+
+    def set_type_id (in):
+        self._type_id = in
+
+    def get_from_id ():
+        return self._from_id
+
+    def set_from_id (in):
+        self._from_id = in
+
+    def get_from_user_id ():
+        return self._from_user_id
+
+    def set_from_user_id (in):
+        self._from_user_id = in
+
+    def get_data ():
+        return self._data
+
+    def set_data (in):
+        self._data = in
+
     def __init__(self, arg):
         super(Message, self).__init__()
         self.arg = arg
--- Peer-to-Peer Chat/Peer.py
+++ Peer-to-Peer Chat/Peer.py
@@ -1,5 +1,48 @@
 class Peer(object):
     """docstring for Peer."""
+
+    _peer_id = ''
+    _next_peer = ''
+    _prev_peer = ''
+    _user_name = ''
+    _peer_ip = ''
+
+    # Getters and setters
+    def get_peer_id():
+        return _peer_id
+    def set_peer_id(in):
+        self._peer_id = in
+    def get_next_peer():
+        return _next_peer
+    def set_next_peer(in):
+        self._next_peer = in
+    def get_pev_peer():
+        return _pev_peer
+    def set_pev_peer(in):
+        self._pev_peer = in
+    def get_user_name():
+        return _user_name
+    def set_user_name(in):
+        self._user_name = in
+    def get_peer_ip():
+        return _peer_ip
+    def set_peer_ip(in):
+        self._peer_ip = in
+
+    def join (ip):
+
+    def exit():
+
+    def initialize():
+
+    def listen():
+
+    def disp_message(data):
+
+    def broadcast(data):
+            
+
+
     def __init__(self, arg):
         super(Peer, self).__init__()
         self.arg = arg
