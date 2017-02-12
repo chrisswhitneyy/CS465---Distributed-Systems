63a3a233ac8dd5151cab5e0af4e0c6b0052bfb0b
Christopher Whitney <chrisswhitneyy@gmail.com>
2017-02-12 14:23:46 -0700

    socket handler thread classes added and list of sockets

--- P2PChat/Node.java
+++ P2PChat/Node.java
@@ -1,31 +1,43 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
 import java.lang.Thread;
 
 public class Node {
-  private Socket _serve_socket;
-  private Socket [] _sockets;
-  private int _node_id;
-  private int _port = 2594;
-  private int _num_connected_nodes;
+  ServerSocket _serve_socket;
+  List<Socket> _sockets;
+  int _node_id;
+  int _port = 2594;
+  int _num_connected_nodes;
 
   // Node constructor
   public void Node(int node_id){
     this._node_id = node_id;
     this._num_connected_nodes = 0;
+    this._sockets = new ArrayList<Socket>(3);
   }
 
   public void start(){
-
-    this._serve_socket = new ServerSocket(this._port);
+    try{
+      this._serve_socket = new ServerSocket(this._port);
+    }
+    catch (IOException error){
+      System.out.println( "Server socket was unable to be created. Error: "+ error);
+    }
 
     System.out.println( "Peer node " + this._node_id + " is running...");
 
     // server loop: infinitely loops and accepting clients
     while (true) {
-      // nesting of instantiation makes it impossible for race conditions
-      (new Thread(new EchoServerThread(serverSocket.accept(),_num_connected_nodes))).start();
-      _num_connected_nodes++;
+      try{
+        // nesting of instantiation makes it impossible for race conditions
+        (new Thread(new SocketHandlerThread(_serve_socket.accept(),_num_connected_nodes))).start();
+        _num_connected_nodes++;
+      }
+      catch (IOException error){
+        System.out.println( "Unable to accept connection. Error: "+ error);
+      }
+
     }
 
   }
