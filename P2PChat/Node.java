80af8fba0676817c7d533ed9d6abdaeacf21e2ea
Christopher Whitney <chrisswhitneyy@gmail.com>
2017-02-15 19:02:46 -0700

    accepted socket added to a list in node

--- P2PChat/Node.java
+++ P2PChat/Node.java
@@ -4,26 +4,28 @@ import java.util.*;
 import java.lang.Thread;
 
 public class Node {
-  ServerSocket _serve_socket;
+  ServerSocket _server_socket;
   List<Socket> _sockets;
+
+  int max_num_con = 4;
   int _node_id;
   int _port = 2594;
   int _num_connected_nodes;
 
   // Node constructor
-  public void Node(int node_id){
-    this._node_id = node_id;
+  public Node(){
+    //this._node_id = id;
     this._num_connected_nodes = 0;
-    this._sockets = new ArrayList<Socket>(3);
+    this._sockets = new ArrayList<Socket>(this.max_num_con);
   }
    public static void main (String[] args){
      Node node = new Node();
-     node.start();
+     node.startServer();
    }
 
-  public void start(){
+  public void startServer(){
     try{
-      this._serve_socket = new ServerSocket(this._port);
+      this._server_socket = new ServerSocket(this._port);
     }
     catch (IOException error){
       System.out.println( "Unable to create server socket. Error: "+ error);
@@ -35,7 +37,7 @@ public class Node {
     while (true) {
       try{
         // nesting of instantiation makes it impossible for race conditions
-        (new Thread(new SocketHandlerThread(_serve_socket.accept(),_num_connected_nodes))).start();
+        (new Thread(new SocketHandlerThread( _server_socket.accept() ))).start();
         _num_connected_nodes++;
       }
       catch (IOException error){
--- P2PChat/SocketHandlerThread.java
+++ P2PChat/SocketHandlerThread.java
@@ -3,31 +3,37 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-class SocketHandlerThread extends Node implements Runnable{
+public class SocketHandlerThread extends Node implements Runnable{
   // class level variables for the socket of the client, a buffer to read in
   // the clients message and an output stream to send a message to the client
   Socket client;
-  int   _num_connected_nodes;
   BufferedReader clientReader;
   DataOutputStream clientWriter;
 
-  public SocketHandlerThread(Socket client,int _num_connected_nodes) {
+  public SocketHandlerThread(Socket client) {
     super._sockets.add(client);
     this.client = client;
-    this._num_connected_nodes = _num_connected_nodes;
   }
+
   //@override
   public void run(){
 
     try{
+
       this.clientReader = new BufferedReader(new InputStreamReader(this.client.getInputStream()));
       this.clientWriter = new DataOutputStream(this.client.getOutputStream());
 
       String message = null;
+
       while ((message = clientReader.readLine()) != null) {
+
           System.out.println("Connection "+ _num_connected_nodes + " said: " + message);
+
           clientWriter.writeBytes(message);
+
       }
+
+
     }catch (IOException e){
 
     }
